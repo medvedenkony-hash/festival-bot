ca0f4a6aa3b0748628c8a86d473bd8950a2bdcdd
medvedenkony-hash <medvedenkony@gmail.com>
2026-05-12 11:48:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -38,3 +38,53 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Server started on port ' + PORT);
 });
+const TELEGRAM_TOKEN = '8591260825:AAEXQTjD3NYmTYy9nuWkSvKWq6tVIZpyH7E';
+const CHAT_ID = '8339993500';
+async function sendTelegram(text) {
+  await fetch(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      chat_id: CHAT_ID,
+      text
+    })
+  });
+}
+app.post('/buy', async (req, res) => {
+  const data = req.body;
+
+  // запись в таблицу
+  await fetch('https://script.google.com/macros/s/AKfycbx5ajjce9nz4__FeYD3sDmhK44GRFTIt-OCflMvQoQ5gdsDacjffqVgPhHJQIhkgpPv/exec', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      ...data,
+      status: 'оплачено'
+    })
+  });
+
+  // телеграм
+  await sendTelegram(
+    `Новая оплата:
+Имя: ${data.name}
+Телефон: ${data.phone}
+Место: ${data.row}-${data.seat}
+Цена: ${data.price}`
+  );
+
+  res.json({ success: true });
+});
+app.post('/buy', async (req, res) => {
+  const data = req.body;
+
+  // запись в таблицу
+  await fetch('https://script.google.com/macros/s/AKfycbx5ajjce9nz4__FeYD3sDmhK44GRFTIt-OCflMvQoQ5gdsDacjffqVgPhHJQIhkgpPv/exec', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      ...data,
+      status: 'оплачено'
+    })
+  });
+
+ 
