f6ece265694f64da162dee539e04baf0abac5482
medvedenkony-hash <medvedenkony@gmail.com>
2026-05-18 19:36:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const TELEGRAM_TOKEN = 'ВСТАВЬ_НОВЫЙ_ТОКЕН';
 const CHAT_ID = '8339993500';
 
 async function sendTelegram(text) {
-  await fetch("https://script.google.com/macros/s/AKfycbyV80Sh7CMfAgT37eeRtnKjyP8smn5EU0uI92c6PrDms4uj-K6jDgDBqTsq2Q_ApmbQ/exec"{
+  await fetch("https://script.google.com/macros/s/AKfycbyn7SDSbTKs3T8obA4XMt_UtuXpfuUb4vy-NzBaDV3mgnN2BpWbSwdLRy96HGBfX6Kq/exec"{
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
@@ -31,7 +31,7 @@ app.post('/new-user', async (req, res) => {
   try {
     const data = req.body;
 
-   const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxK2ya-FkkP2W00ohmdJD2ve4k7veB02qDeYx_wV5H_3NwewxV2-aW0DVffAGOyGn0L/exec";
+   const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyn7SDSbTKs3T8obA4XMt_UtuXpfuUb4vy-NzBaDV3mgnN2BpWbSwdLRy96HGBfX6Kq/exec";
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data)
